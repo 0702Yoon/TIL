c621c50d33e6099bf13a94487dec2a92828e2251
YOON <120346721+0702Yoon@users.noreply.github.com>
2023-07-07 22:40:06 +0900

    Update PMainFrane.java
    로그인창 구현.

--- swingSugang/main/PMainFrane.java
+++ swingSugang/main/PMainFrane.java
@@ -1,6 +1,9 @@
 package main;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.FileNotFoundException;
+import java.util.Objects;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
@@ -10,19 +13,37 @@ import presentation.PSugangsincheng;
 import valueObject.VUserInfo;
 
 
-public class PMainFrane extends JFrame{
+public class PMainFrame extends JFrame{
 	// constructor
 	private Scanner keyboard;
 	private PLogin pLogin;
 	private PSugangsincheng pSugangsincheong;
-	public PMainFrane() {
+	public PMainFrame() {
 		
 		this.setSize(400,600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		pSugangsincheong = new PSugangsincheng();
-		this.add(pSugangsincheong);
 		
+		pLogin = new PLogin();
+		pLogin.getButtone().addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent event) {
+				if(event.getClickCount()==1) {
+					VUserInfo vUserInfo = pLogin.login();
+					if(Objects.isNull(vUserInfo)) { // null인지 아닌 지 확인 해주는 좋은 메소드다.
+						
+					}
+					else {
+						add(pSugangsincheong);
+					}
+				
+				}
+			}
+		});
+		
+		add(pLogin);
+//		add(pSugangsincheong); 원래 이렇게 해놓고 안보이게 했었는 데 왜 안됐을 까?
+	
 	}
 	
 		
@@ -31,7 +52,7 @@ public class PMainFrane extends JFrame{
 		// memory allocation
 		// invoke constructor
 		// object name and memory address binding
-		PMainFrane pMainFrame = new PMainFrane();
+		PMainFrame pMainFrame = new PMainFrame();
 		pMainFrame.setVisible(true);
 	
 	}
